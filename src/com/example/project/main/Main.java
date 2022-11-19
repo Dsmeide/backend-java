cb2ac4dc3bbab75f6c64fd0988693e9ce645ed70
Derek Arends <thinkovator@gmail.com>
2022-11-19 15:35:06 -0600

    added target back

--- src/com/example/project/main/Main.java
+++ src/com/example/project/main/Main.java
@@ -7,7 +7,7 @@ import com.example.project.services.UserService;
 
 class Main {
 	public static void main(String[] args) {
-		IUserRepo userRepo = new UserRepo();
+		IUserRepo userRepo = new UserRepo(); 
 		IUserService userService = new UserService(userRepo);
 		System.out.println("Hello World!"); // Display the string.
 	}
